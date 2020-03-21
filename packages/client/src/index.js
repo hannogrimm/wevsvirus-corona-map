fdea5bb17589e6b8bd4d6b17fe8ab9dcdb517e6e
Svvite <dimitrigrimm@yahoo.de>
2020-03-21 21:53:01 +0100

    Fix App.js missing

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,16 +1,16 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "./i18n";
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import * as serviceWorker from './serviceWorker'
+import './i18n'
 
-require("dotenv").config();
+require('dotenv').config()
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading... </div>}>
     <App />
   </Suspense>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
