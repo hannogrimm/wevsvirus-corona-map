d5b3be5834bb7421f481006f681b52c76eeec997
Lucca Doerrstein <doerrstein.lucca@yahoo.de>
2020-03-21 19:27:39 +0100

    changed var to const

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,16 +1,16 @@
-var createError = require("http-errors")
+const createError = require("http-errors")
 
-var express = require("express")
+const express = require("express")
 const connectDB = require("./config/db")
 
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
 
-var indexRouter = require("./routes/index")
-var usersRouter = require("./routes/users")
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
 
-var app = express()
+const app = express()
 
 // Connect Database
 connectDB()
