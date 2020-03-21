344bc74372504944b2197e8fdbdf76060a964cf5
Fabian Volkers <fabian.volkers@gmail.com>
2020-03-21 23:47:56 +0100

    added reverse geocoding when clicking on map

--- packages/client/src/Map.jsx
+++ packages/client/src/Map.jsx
@@ -7,6 +7,32 @@ const API_KEY = process.env.REACT_APP_HERE_API_KEY ?
   process.env.REACT_APP_HERE_API_KEY :
   "unknown";
 
+function reverseGeocode(platform, coord) {
+  var geocoder = platform.getGeocodingService(),
+    reverseGeocodingParameters = {
+      prox: `${coord.lat},${coord.lng}`, // Coords from click event
+      mode: 'retrieveAddresses',
+      maxresults: '1',
+      jsonattributes: 1
+    };
+
+  geocoder.reverseGeocode(
+    reverseGeocodingParameters,
+    onSuccess,
+    onError
+  );
+}
+
+function onSuccess(result) {
+  var locations = result.response.view[0].result;
+  console.log(locations[0].location.address)
+}
+
+
+function onError(error) {
+  alert('Can\'t reach the remote server');
+}
+
 const Map = () => {
   const [lng, setLng] = useState(13);
   const [lat, setLat] = useState(52);
@@ -30,6 +56,7 @@ const Map = () => {
       }
     );
 
+
     var ui = window.H.ui.UI.createDefault(map, defaultLayers);
 
     var mapEvents = new window.H.mapevents.MapEvents(map);
@@ -41,6 +68,7 @@ const Map = () => {
         evt.currentPointer.viewportY);
 
       console.log("Clicked at: ", coord['lat'], coord['lng']);
+      reverseGeocode(platform, coord)
     });
 
 
