dcf24defcb8448696d9d22776ecdb88ae7d8820c
Moritz Wallawisch <wallawitsch@gmail.com>
2020-03-22 12:40:57 +0100

    fixed model

--- packages/server/models/gpspoint.js
+++ packages/server/models/gpspoint.js
@@ -5,20 +5,21 @@ const GPSPointsSchema = mongoose.Schema({
     type: Boolean,
     required: true,
   },
-  location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-    date: {
-      type: Date,
-      default: Date.now,
-    },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+  /* city: {
+    type: String,
+    required: true,
+  }, */
+  postcode: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   },
 })
 
