7cc0ad6bb2c2cb0ae325bf5b9fce36686156af48
Lucca Doerrstein <doerrstein.lucca@yahoo.de>
2020-03-22 19:24:01 +0100

    location lookup works now

--- packages/server/models/gpspoint.js
+++ packages/server/models/gpspoint.js
@@ -5,8 +5,16 @@ const GPSPointsSchema = mongoose.Schema({
     type: String,
     default: 'isInfected', //else: maybeInfected, notInfected (MVP only: "isInfected"!)
   },
+  location: {
+    type: {
+      type: String, 
+      enum: ['Point'], 
+      required: true
+    },
     coordinates: {
-      type: [Number]
+      type: [Number],
+      required: true
+    }
   },
   timeArrival: {
     type: Date
